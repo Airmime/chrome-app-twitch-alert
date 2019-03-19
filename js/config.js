4cf307568c6d135dba2488e055712d05597a78c6
Rémi MARION <r.marion42@hotmail.fr>
2019-03-19 08:56:24 +0100

    Update config.js
    Added Help URL

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 var config = {
   name: "DrDisRespect",
-  clientID: "YOUR_CLIENT_ID",
+  clientID: "YOUR_CLIENT_ID", // How generate client ID ? -> https://docs.aws.amazon.com/lumberyard/latest/userguide/chatplay-generate-twitch-client-id.html
   default_color: "#c23131"
 }
